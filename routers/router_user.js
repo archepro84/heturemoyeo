79ff8b7ff5ced8859b2f5efe41cd87ed6108f448
archepro84 <archepro84@gmail.com>
2021-07-28 15:10:26 +0900

    user API Update
    user target Router를 업데이트 하였습니다.

--- routers/router_user.js
+++ routers/router_user.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require('joi')
 const {Users, sequelize, Sequelize} = require("../models")
 const authmiddleware = require("../middleware/auth-middleware")
 
-router.route('/')
-    .post((req, res) => {
 
-    })
-    .get((req, res) => {
-        res.send({result: "안녕하세요"})
-    })
-
+const userIdSchema = Joi.number().min(1).required();
 router.route('/me')
     .post(authmiddleware, async (req, res) => {
         try {
@@ -37,6 +32,118 @@ router.route('/me')
                         profileImg: result[0].profileImg,
                         statusMessage: result[0].statusMessage,
                         likeItem
+                    })
+                })
+        } catch (error) {
+            res.status(401).send(
+                {errorMessage: "정보를 찾을 수 없습니다."}
+            )
+        }
+    })
+
+router.route('/target/post')
+    .get(authmiddleware, async (req, res) => {
+        try {
+            const userId = res.locals.user.userId;
+            const targetUserId = await userIdSchema.validateAsync(
+                Object.keys(req.query).length ? req.query.userId : req.body.userId
+            );
+            if (userId == targetUserId) {
+                res.status(412).send({errorMessage: "동일한 아이디를 조회할 수 없습니다."})
+                return;
+            }
+            const query = `
+                SELECT nickname, rating, profileImg, statusMessage, 
+                (SELECT GROUP_CONCAT(likeItem ORDER BY likeItem ASC SEPARATOR ', ')
+                    FROM Likes 
+                    WHERE userId = u.userId
+                    GROUP BY userId) AS likeItem,
+                (SELECT COUNT(*)  
+                    FROM (SELECT DISTINCT postId FROM Channels WHERE userId = ${userId}) AS a
+                    JOIN (SELECT DISTINCT postId FROM Channels WHERE userId = ${targetUserId} ) AS b
+                    ON a.postId = b.postId) AS scheduleCount,
+                (SELECT title FROM Posts WHERE postId = (SELECT a.postId
+                    FROM (SELECT DISTINCT postId FROM Channels WHERE userId = ${userId}) AS a
+                    JOIN (SELECT DISTINCT postId FROM Channels WHERE userId = ${targetUserId}) AS b
+                    ON a.postId = b.postId ORDER BY a.postId DESC LIMIT 1)) AS scheduleTitle,
+                (SELECT COALESCE(MIN('Y'), 'N')
+                    FROM Friends
+                    WHERE EXISTS (select a.1
+                        FROM (SELECT DISTINCT 1
+                            FROM  Friends
+                            WHERE giveUserId  = ${userId} AND receiveUserId  = ${targetUserId}) AS a
+                        JOIN (SELECT DISTINCT 1
+                            FROM  Friends
+                            WHERE giveUserId  = ${targetUserId} AND receiveUserId  = ${userId}) AS b)) AS isFriend
+                FROM Users AS u
+                WHERE userId = ${targetUserId}`
+
+            // 검색된 데이터가 없을 경우 에러가 발생한다.
+            await sequelize.query(query, {type: Sequelize.QueryTypes.SELECT})
+                .then((result) => {
+                    let likeItem = Array();
+                    if (result[0].likeItem)
+                        for (const Item of result[0].likeItem.split(', '))
+                            likeItem.push(Item)
+                    res.send({
+                        nickname: result[0].nickname,
+                        rating: result[0].rating,
+                        profileImg: result[0].profileImg,
+                        statusMessage: result[0].statusMessage,
+                        likeItem,
+                        scheduleCount: result[0].scheduleCount,
+                        scheduleTitle: result[0].scheduleTitle,
+                        isFriend: result[0].scheduleTitle == 'Y' ? true : false,
+                    })
+                })
+        } catch (error) {
+            res.status(401).send(
+                {errorMessage: "정보를 찾을 수 없습니다."}
+            )
+        }
+    })
+router.route('/target/friend')
+    .get(authmiddleware, async (req, res) => {
+        try {
+            const userId = res.locals.user.userId;
+            const targetUserId = await userIdSchema.validateAsync(
+                Object.keys(req.query).length ? req.query.userId : req.body.userId
+            );
+            if (userId == targetUserId) {
+                res.status(412).send({errorMessage: "동일한 아이디를 조회할 수 없습니다."})
+                return;
+            }
+
+            const query = `
+                SELECT u.userId, u.email, u.name, u.nickname, u.profileImg, u.statusMessage,
+                (SELECT GROUP_CONCAT(likeItem ORDER BY likeItem ASC SEPARATOR ', ')
+                    FROM Likes 
+                    WHERE userId = u.userId
+                    GROUP BY userId) AS likeItem
+                FROM Users AS u
+                WHERE userId = ${targetUserId} AND 'Y' = (SELECT COALESCE(MIN('Y'), 'N')
+                    FROM Friends
+                    WHERE EXISTS (select a.1
+                        FROM (SELECT DISTINCT 1
+                            FROM  Friends
+                            WHERE giveUserId  = ${targetUserId} AND receiveUserId  = ${userId}) AS a
+                        JOIN (SELECT DISTINCT 1
+                            FROM  Friends
+                            WHERE giveUserId = ${userId} AND receiveUserId  = ${targetUserId}) AS b))`
+
+            // 검색된 데이터가 없을 경우 에러가 발생한다.
+            await sequelize.query(query, {type: Sequelize.QueryTypes.SELECT})
+                .then((result) => {
+                    let likeItem = Array();
+                    if (result[0].likeItem)
+                        for (const Item of result[0].likeItem.split(', '))
+                            likeItem.push(Item)
+                    res.send({
+                        nickname: result[0].nickname,
+                        rating: result[0].rating,
+                        profileImg: result[0].profileImg,
+                        statusMessage: result[0].statusMessage,
+                        likeItem,
                     })
                 })
         } catch (error) {
