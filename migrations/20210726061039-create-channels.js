44f5090869d98bccb3ac4560ed65e1728b113741
archepro84 <archepro84@gmail.com>
2021-08-19 16:09:22 +0900

    archepro84 : Posts View Update

--- migrations/20210726061039-create-channels.js
+++ migrations/20210726061039-create-channels.js
@@ -40,6 +40,19 @@ module.exports = {
                 type: Sequelize.DATE,
                 defaultValue: Sequelize.fn('now')
             }
+        }).then(() => {
+            const POSTS_VW_Query = `
+                CREATE VIEW POSTS_VW
+                AS
+                    SELECT p.postId, p.userId, p.title, p.postImg, 
+                        (SELECT COUNT(confirm) FROM Channels WHERE postId = p.postId AND confirm = 1) AS confirmCount,
+                        COUNT(*) AS currentMember, p.maxMember, p.startDate, p.endDate, p.place,
+                        (SELECT GROUP_CONCAT(tag ORDER BY tag ASC SEPARATOR ', ') FROM Tags WHERE postId = p.postId GROUP BY postId) AS tagItem
+                    FROM Channels  AS c
+                    JOIN Posts AS p
+                    ON p.postId = c.postId
+                    GROUP BY c.postId `
+            queryInterface.sequelize.query(POSTS_VW_Query)
         });
     },
     down: async (queryInterface, Sequelize) => {
